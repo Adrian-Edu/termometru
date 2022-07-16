3ec4a818ef5c4dbb7d6c7832bc2b000350cb6349
Adrian-Edu <103953194+Adrian-Edu@users.noreply.github.com>
2022-07-16 18:52:17 +0300

    reiuau

--- src/App.jsx
+++ src/App.jsx
@@ -4,29 +4,18 @@ import './App.css';
 
 function App() {
 
-  const [valoareTemperatura, setValoareTemperatura]= useState(10)
-
-
-  const increse = () => {
-   setValoareTemperatura(valoareTemperatura + 1)
-  }
-
-  const descrise = () => {
-    setValoareTemperatura(valoareTemperatura - 1)
-  }
-
   return (
     <div className="App">
       <header className="App-header">
         <div className='container'>
 
-        <div className={'display-temperature ${blue}' }>
-        <h1>{valoareTemperatura}</h1>
+        <div className="display-temperature">
+        <h1>Salut</h1>
         </div>
 
         <div className="buttonul">
-        <button onClick={increse }>+</button>
-        <button onClick={descrise}>-</button>
+        <button>+</button>
+        <button>-</button>
         </div>
 
         </div>  
