78670d51cabe2ededffc642da3f2a677d4c42359
Adrian-Edu <103953194+Adrian-Edu@users.noreply.github.com>
2022-07-18 08:37:14 +0300

    Bravo

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,39 @@ import './App.css';
 
 function App() {
 
+  const [click, setClick] = useState(0)
+  const [change, setChange] = useState("")
+
+  const creste = () => {
+     const plus = click + 1 
+    
+     if (plus >= 15) {
+      setChange("hot")
+     }
+     setClick(plus)
+  }
+ 
+  const descreste = () => {
+    const minus = click - 1 
+   
+    if (minus < 15) {
+      setChange("freeze")
+    }
+    setClick(minus)
+ }
+
   return (
     <div className="App">
       <header className="App-header">
         <div className='container'>
 
-        <div className="display-temperature">
-        <h1>Nimic</h1>
+        <div className={`display-temperature ${change}`}>
+        <h1>{click}</h1>
         </div>
 
         <div className="buttonul">
-        <button>-</button>
-        <button>+</button>
+        <button onClick={descreste}>-</button>
+        <button onClick={creste}>+</button>
         </div>
 
         </div>  
